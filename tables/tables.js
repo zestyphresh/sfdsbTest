9cd8ade5f234c022dbaddc3fc49dce69fdaae7c3
zestyphresh <zestyphresh@gmail.com>
2014-08-10 17:04:04 +0000

    sat

--- tables/tables.js
+++ tables/tables.js
@@ -60,27 +60,27 @@ var TABLE_COUNTDOWN = (function($t) {
                             _modpriv.returnDefs([2], '0,0', 'alignRight'),
                             _modpriv.returnDefs([4], '0%', 'alignRight'),
             ],
-            'footerCallback' : function (tfoot, data, start, end, display) {
-                var api = this.api();
-                
-                console.log(api.column(1), api.column(2));
-
-                var totalValue = api.column(1).data().reduce(function (a, b) {
-                    return a + b;
-                })
-                var totalQuantity = api.column(2).data().reduce(function (a, b) {
-                    return a + b;
-                })
-                var totalVsTarget = -totalTarget + totalValue;
-                var vsTargetPercentage = totalValue / totalTarget;
-                
-                $j(api.column(0).footer()).html('Total');
-                $j(api.column(1).footer()).html(numeral(totalValue).format('$0,0'));
-                $j(api.column(2).footer()).html(numeral(totalQuantity).format('0,0'));              
-                $j(api.column(3).footer()).html(numeral(totalVsTarget).format('$0,0'));
-                $j(api.column(4).footer()).html(numeral(vsTargetPercentage).format('0%'));
-            }
+            'footerCallback' : footerCallback
         });
+        
+        function footerCallback (tfoot, data, start, end, display) {
+            var api = this.api();
+
+            var totalValue = api.column(1).data().reduce(function (a, b) {
+                return a + b;
+            })
+            var totalQuantity = api.column(2).data().reduce(function (a, b) {
+                return a + b;
+            })
+            var totalVsTarget = -totalTarget + totalValue;
+            var vsTargetPercentage = totalValue / totalTarget;
+            
+            $j(api.column(0).footer()).html('Total');
+            $j(api.column(1).footer()).html(numeral(totalValue).format('$0,0'));
+            $j(api.column(2).footer()).html(numeral(totalQuantity).format('0,0'));              
+            $j(api.column(3).footer()).html(numeral(totalVsTarget).format('$0,0'));
+            j(api.column(4).footer()).html(numeral(vsTargetPercentage).format('0%'));
+        }
 
         return { 
             reload : function(data) { table.clear().rows.add(data).draw(); } 
