e2f527cb8aacb6fccb96910b6392e9f059531de1
zestyphresh <zestyphresh@gmail.com>
2014-08-13 11:10:31 +0000

    regwegerg

--- models/headline-opportunities.js
+++ models/headline-opportunities.js
@@ -87,88 +87,6 @@ var MODEL_OPPORTUNITIES = (function($m) {
         };
         
         //PRIVATE FUNCTIONS
-        //TODO - Not adding records correctly by week plus need way to push week 53 to week 1
-        function _dataTransformToWeeks(originalData) {
-            
-            var deliveryWeeks = 4,
-                storeWeeks = 4,
-                newData = [];
-    
-            _(originalData).each(function(d) {
-
-                var index = _modpriv.datesByDate[d.closeDate].dateIndex;
-                
-                var headline = d.recordType === 'Headline' ? true : false;
-    
-                var thisWeek = $j.extend({}, d);
-                    thisWeek.type = 'Live';
-                    newData.push(thisWeek);
-                
-                if(d.recordType == 'Headline') {
-                    
-                    for (var i = 1; i <= deliveryWeeks; i++) {
-                        
-                        var newIndex = index - (i*7);
-                        
-                        while (_modpriv.datesByIndex[newIndex].cyWeekNum == 53) {newIndex--;}
-
-                        var delWeek = $j.extend({}, d);
-                            delWeek.week = _modpriv.datesByIndex[newIndex].fyYearWeek;
-                            delWeek.month = _modpriv.datesByIndex[newIndex].fyYearMonth;
-                            delWeek.closeDate = _modpriv.datesByIndex[newIndex].cyDate;
-                            delWeek.weeklyValue = 0;
-                            delWeek.type = 'Delivery';
-                        newData.push(delWeek);
-                
-                    }
-    
-                    for (var i = 1; i <= storeWeeks; i++) {
-                        
-                        while (_modpriv.datesByIndex[newIndex].cyWeekNum == 53) {newIndex++;}
-                        
-                        var newIndex = index + (i*7);
-
-                        var storeWeek = $j.extend({}, d);
-                            storeWeek.week = _modpriv.datesByIndex[newIndex].fyYearWeek;
-                            storeWeek.month = _modpriv.datesByIndex[newIndex].fyYearMonth;
-                            storeWeek.closeDate = _modpriv.datesByIndex[newIndex].cyDate;
-                            storeWeek.type = 'In Store';
-                        newData.push(storeWeek);
-                    
-                    }
-                
-                }
-                
-                if (!d.isPromotion) {
-                
-                    var maxIndex = 1826; 
-                    var add = headline ? storeWeeks * 7 : 0;
-                    var start = index + add;
-                    var remainingWeeks = Math.floor((maxIndex - start) / 7);
-
-                    for (var i = 1; i <= remainingWeeks; i++) {
-                        
-                        var newIndex = start + (i*7);
-                        
-                        while (_modpriv.datesByIndex[newIndex].cyWeekNum == 53) {newIndex++;}
-                    
-                        var saleWeek = $j.extend({}, d);
-                            saleWeek.week = _modpriv.datesByIndex[newIndex].fyYearWeek;
-                            saleWeek.month = _modpriv.datesByIndex[newIndex].fyYearMonth;
-                            saleWeek.closeDate = _modpriv.datesByIndex[newIndex].cyDate;
-                            saleWeek.type = headline ? 'Sales' : 'Loss';
-                        newData.push(saleWeek);
-                    
-                    }
-                
-                }
-                
-            });
-                
-            return newData;
-            
-        }
-        
         function _dataTransformToMonthlySales(originalData) {
             
             var maxDate = new moment('2014-12-31', 'YYYY-MM-DD');
@@ -177,22 +95,18 @@ var MODEL_OPPORTUNITIES = (function($m) {
             _(originalData).each(function(d) {
 
                 var headline = d.recordType === 'Headline' ? true : false;
-    
-                var thisMonth = $j.extend({}, d);
-                    thisMonth.value = headline? d.isoValue : d.annualisedValue/12;
-                    thisMonth.date = d.mDate.format('YYYY-MM');
-                    newData.push(thisMonth);
+
+                var value = headline? d.isoValue : d.annualisedValue/12;
+
+                newData.push({'date' : d.mDate.format('YYYY-MM'), 'value' : value, 'recordType' : d.recordType})
                 
                 d.mDate.add('months', 1);    
                     
                 if (!d.isPromotion) {
 
                     while (d.mDate < maxDate) {
-                        
-                        var thisMonth = $j.extend({}, d);
-                            thisMonth.value = d.annualisedValue/12;
-                            thisMonth.date = d.mDate.format('YYYY-MM');
-                        newData.push(thisMonth);
+
+                        newData.push({'date' : d.mDate.format('YYYY-MM'), 'value' : d.annualisedValue/12, 'recordType' : d.recordType})
                         
                         d.mDate.add('months', 1);
                     }
