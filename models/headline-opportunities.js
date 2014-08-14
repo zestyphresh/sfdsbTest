4f5bd7fe4cb748c7b489712e2ca99de79fa02b1f
zestyphresh <zestyphresh@gmail.com>
2014-08-14 12:50:53 +0000

    hguivobl

--- models/headline-opportunities.js
+++ models/headline-opportunities.js
@@ -181,7 +181,7 @@ var MODEL_OPPORTUNITIES = (function($m) {
                 
             });
             
-            _.sortBy(newData, 'start');
+            _.sortBy(newData, function(v) { return v.start; });
                 
             //console.log('Function:_dataTransformToTimeline2',newData);  
                 
