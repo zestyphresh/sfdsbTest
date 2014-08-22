f699290bb9e5b6b95b0c619272ea0326526ad594
zestyphresh <zestyphresh@gmail.com>
2014-08-22 11:18:52 +0000

    ssj

--- models/headline-opportunities.js
+++ models/headline-opportunities.js
@@ -88,7 +88,7 @@ var MODEL_OPPORTUNITIES = (function($m) {
         getData.filtered = function(threatsNegative) { return _dataAll; };
         getData.timeline = function() { return _dataTransformToTimeline(_dataAll); };
         getData.monthlySales = function() { return _dataTransformToMonthlySales(_dataAll); };
-        getData.oppsByStageCategory = function(stage) { return _.filter(_convertThreatsToNegative(_dataAll), {'stageCategory' : stage}); };
+        getData.oppsByStageCategory = function(stage) { return _.filter(_dataAll, {'stageCategory' : stage}); };
         
         return { 
             fetch : fetch,
