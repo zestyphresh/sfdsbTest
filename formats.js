b6764af1ddca1856496e274a75bca8b96bf11705
zestyphresh <zestyphresh@gmail.com>
2014-08-22 09:00:00 +0000

    ssjhjhj

--- formats.js
+++ formats.js
@@ -3,7 +3,7 @@ var f = {
     toGbp : function(num) { return accounting.formatMoney(num); },
     
     toGbpWithComparison : function(num1, num2, cls) {
-        return accounting.formatMoney(num1) + ' (<span class="' + cls + '>' + accounting.formatMoney(num2) = '</span>)';
+        return accounting.formatMoney(num1) + ' (<span class="' + cls + '>' + accounting.formatMoney(num2) + '</span>)';
     }
 
 };
