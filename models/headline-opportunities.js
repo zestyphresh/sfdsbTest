afb1846f7154805123e6664b8d712af55c2c1ae7
zestyphresh <zestyphresh@gmail.com>
2014-08-26 08:01:39 +0000

    ssgg

--- models/headline-opportunities.js
+++ models/headline-opportunities.js
@@ -67,7 +67,7 @@ var MODEL_OPPORTUNITIES = (function($m) {
                     v.weeklyValue = -v.weeklyValue;
                     v.weeklyValuePrevious = -v.weeklyValuePrevious;
                 }
-            });
+            }).values();
             
             console.log(result);
             return result;
@@ -88,7 +88,7 @@ var MODEL_OPPORTUNITIES = (function($m) {
             
         }
 
-        getData.filtered = function(threatsNegative) { return _dataAll; };
+        getData.filtered = function() { return _dataAll; };
         getData.timeline = function() { return _dataTransformToTimeline(_dataAll); };
         getData.monthlySales = function() { return _dataTransformToMonthlySales(_dataAll); };
         getData.oppsByStageCategory = function(stage) { return _.filter(_convertThreatsToNegative(_dataAll), {'stageCategory' : stage}); };
