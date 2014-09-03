991ef240f30a54007ae36f67d5b9694d09d1ab41
zestyphresh <zestyphresh@gmail.com>
2014-09-03 08:45:55 +0000

    sshhj

--- templates.js
+++ templates.js
@@ -100,7 +100,7 @@ _templates['heading'] = ""+
 _templates['epos-basic'] = ""+
         "<div class='row'>"+
         ""+    
-        "    <div class='col-xs-12 col-md-3'>"+
+        "    <div class='col-xs-12'>"+
         "        <div class='panel panel-default'>"+
         "            <div class='panel-heading'>" +
         "                <span>Filters</span>" +
